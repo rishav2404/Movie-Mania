61b25d107adea8bbb146a7932c7a6f36747f2098
rishav2404 <rishavsaha24042003@gmail.com>
2024-03-17 00:11:15 +0530

    upgraded the version of libraries

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
-import {store} from './store/store'
+import {store} from './store/store.js'
 import {Provider} from 'react-redux'
 
 ReactDOM.createRoot(document.
